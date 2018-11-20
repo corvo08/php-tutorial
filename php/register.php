5e28d38d3116c261832559cc4610e2a921511ec3
root <root@ip-172-31-93-160.ec2.internal>
2018-11-20 18:34:12 +0000

    11-20-18 1:33PM

--- php/register.php
+++ php/register.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<h2>Registration Page</h2>
-	<a href="index.php">Click here to go back<br /> <br />
+	<a href="index.php">Click here to go back </a><br /> <br />
 	<form action="register.php" method="POST">
 		Enter Username: <input type="text" name="username" required="required" /> <br />
 		Enter Password: <input type="password" name="password" required="required" /> <br />
