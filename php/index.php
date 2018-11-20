eb9ee5b3c2b2cc8dfd6b5fab42ce4450757cd7c7
root <root@ip-172-31-93-160.ec2.internal>
2018-11-20 18:24:49 +0000

    11-20-18 1:24PM

---
+++ php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First PHP Website</title>
+</head>
+<body>
+	<h2>Welcome to my first PHP Website!!!</h2>
+	<a href="login.php"> Click here to login <br />
+	<a href="register.php"> Click here to register
+</body>
+</html>
---
+++ php/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First PHP Website</title>
+</head>
+<body>
+	<h2>Registration Page</h2>
+	<a href="index.php">Click here to go back<br /> <br />
+	<form action="register.php" method="POST">
+		Enter Username: <input type="text" name="username" required="required" /> <br />
+		Enter Password: <input type="password" name="password" required="required" /> <br />
+		<input type="submit" value="Register">
+	</form>
+
+</body>
+</html>
