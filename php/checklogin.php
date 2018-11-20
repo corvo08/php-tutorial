cd9e4a3f56d764839763167f38e37a1ae3dd0653
root <root@ip-172-31-93-160.ec2.internal>
2018-11-20 20:30:21 +0000

    11-20-18 3:30PM

--- php/checklogin.php
+++ php/checklogin.php
@@ -9,30 +9,24 @@
 	$exists = mysqli_num_rows($query); //checks if usrname exists
 	$table_users = "";
 	$table_password = "";
-	 if($exists > 0) //IF there are no returning rows or no existing username
-    {
-       while($row = mysqli_fetch_assoc($query)) // display all rows from query
-       {
-          $table_users = $row['username']; // the first username row is passed on to $table_users, and so on until the query is finished
-          $table_password = $row['password']; // the first password row is passed on to $table_password, and so on until the query is finished
-       }
-       if(($username == $table_users) && ($password == $table_password))// checks if there are any matching fields
-       {
-          if($password == $table_password)
-          {
-             $_SESSION['user'] = $username; //set the username in a session. This serves as a global variable
-             header("location: home.php"); // redirects the user to the authenticated home page
-          }
-       }
-       else
-       {
-        Print '<script>alert("Incorrect Password!");</script>'; // Prompts the user
+	if ($exists > 0) { //if there are no returning rows or no existing username
+		while($row = mysqli_fetch_assoc($query)) { //display all rows from query
+			$table_users = $row['username']; //the first username row is passed on to $table_users until the query is done
+			$table_password = $row['password']; //essentially same as username, but password table
+		} 
+		if(($username == $table_user) && ($password == $table_password)) { //checks for matching fields
+			if ($password == $table_password) {
+				$_SESSION['user'] = $username; //sets the username in this session. global variable.
+				header("location:home.php"); //redirects user to the authenticated home page
+			}
+		}
+		else {
+			Print '<script>alert("Incorrect Password!");</script>'; // Prompts the user
+        	Print '<script>window.location.assign("login.php");</script>'; // redirects to login.php
+		}
+	}
+	else {
+		Print '<script>alert("Incorrect username!");</script>'; // Prompts the user
         Print '<script>window.location.assign("login.php");</script>'; // redirects to login.php
-       }
-    }
-    else
-    {
-        Print '<script>alert("Incorrect username!");</script>'; // Prompts the user
-        Print '<script>window.location.assign("login.php");</script>'; // redirects to login.php
-    }
-?>
+	}
+	?>
